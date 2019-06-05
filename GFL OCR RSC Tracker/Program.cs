c86ca32014dd9069f00c1b156f940f4ebe3926ee
kyler-workman <workmkyler@gmail.com>
2019-06-05 02:11:47 -0600

    Uses better OCR library, actually gets numbers from image
    Fuck IronOCR, removes it in favor of Tesseract

--- GFL OCR RSC Tracker/Program.cs
+++ GFL OCR RSC Tracker/Program.cs
@@ -1,6 +1,6 @@
-﻿using IronOcr;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tesseract;
 
 namespace GFL_OCR_RSC_Tracker
 {
@@ -18,41 +19,48 @@ namespace GFL_OCR_RSC_Tracker
         {
             Application.EnableVisualStyles();
             BoundsCapturer b = new BoundsCapturer();
-            b.DoYourShit();
-
-            Console.WriteLine("starting parsing");
-            ReadAndParse();
+            b.CreateFinder();
+            while (Math.PI > Math.E)
+            {
+                Console.WriteLine("starting parsing");
+                Console.WriteLine(ReadAndParse());
+                Console.WriteLine("Press a key to repeat");
+                Console.ReadKey();
+                b.RecaptureArea();
+            }
         }
 
-        private static void ReadAndParse()
+        private static string ReadAndParse()
         {
-            var Ocr = new AdvancedOcr()
-            {
-                DetectWhiteTextOnDarkBackgrounds = true
-            };
+            var testImagePath = "Capture.png";
+            StringBuilder FinalText = new StringBuilder();
 
-            var testDocument = @"Capture.png";
-            var Results = Ocr.Read(testDocument);
-            Console.WriteLine(Results.Text);
+            var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+            var img = Pix.LoadFromFile(testImagePath);
+            var page = engine.Process(img);
+            var text = page.GetText();
+            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+            return text;
         }
     }
 
     class BoundsCapturer
     {
-        Rectangle generatedBounds;
-        bool hangOnCapturedBound;
+        private Rectangle generatedBounds;
+        public bool hangOnCapturedBound { get; set; }
 
-        public BoundsCapturer(bool hangOnCapturedBound=false)
+        public BoundsCapturer(bool hangOnCapturedBound = false)
         {
             this.hangOnCapturedBound = hangOnCapturedBound;
         }
 
-        public void DoYourShit()
+        public void CreateFinder()
         {
             var boundsFinder = new Form
             {
-                Width=882,
-                Height=103,
+                Width = 882,
+                Height = 103,
                 BackColor = Color.Firebrick,
                 TransparencyKey = Color.Firebrick,
                 Text = "Drag this to capture bounds, close to capture area, this can also be resized"
@@ -63,6 +71,18 @@ namespace GFL_OCR_RSC_Tracker
             if (hangOnCapturedBound) ShowCapture();
             Console.WriteLine(generatedBounds);
         }
+        public void RecaptureArea()
+        {
+            if (generatedBounds != null)
+            {
+                CaptureArea();
+                if (hangOnCapturedBound) ShowCapture();
+            }
+            else
+            {
+                throw new InvalidOperationException("Use CreateFinder to determine bounds first");
+            }
+        }
 
         private void CaptureArea()
         {
@@ -71,17 +91,20 @@ namespace GFL_OCR_RSC_Tracker
             g.CopyFromScreen(generatedBounds.X, generatedBounds.Y, 0, 0, generatedBounds.Size);
 
 
+            //butchers this fucking image
+
             for (int x = 0; x < captureBmp.Width; x++)
             {
                 for (int y = 0; y < captureBmp.Height; y++)
                 {
                     Color pixelColor = captureBmp.GetPixel(x, y);
-                    Color newColor = Color.FromArgb(pixelColor.R > 128 ? 255 : 0, pixelColor.R > 128 ? 255 : 0, pixelColor.R > 128 ? 255 : 0);
+                    int value = pixelColor.R > 220 && pixelColor.B < 20 ? 255 : 0;
+                    Color newColor = Color.FromArgb(value, value, value);
                     captureBmp.SetPixel(x, y, newColor);
                 }
             }
 
-            captureBmp.Save("Capture.png", ImageFormat.Png);
+            captureBmp.Save("Capture.png", System.Drawing.Imaging.ImageFormat.Png);
         }
 
         private void ShowCapture()
