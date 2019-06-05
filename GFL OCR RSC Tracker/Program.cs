655a1a1bb8490f86586dd8f9e7d70e35429e4002
kyler-workman <workmkyler@gmail.com>
2019-06-05 00:51:42 -0600

    add first ocr trial

--- GFL OCR RSC Tracker/Program.cs
+++ GFL OCR RSC Tracker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IronOcr;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -18,8 +19,22 @@ namespace GFL_OCR_RSC_Tracker
             Application.EnableVisualStyles();
             BoundsCapturer b = new BoundsCapturer();
             b.DoYourShit();
+
+            Console.WriteLine("starting parsing");
+            ReadAndParse();
         }
 
+        private static void ReadAndParse()
+        {
+            var Ocr = new AdvancedOcr()
+            {
+                DetectWhiteTextOnDarkBackgrounds = true
+            };
+
+            var testDocument = @"Capture.png";
+            var Results = Ocr.Read(testDocument);
+            Console.WriteLine(Results.Text);
+        }
     }
 
     class BoundsCapturer
@@ -36,14 +51,17 @@ namespace GFL_OCR_RSC_Tracker
         {
             var boundsFinder = new Form
             {
+                Width=882,
+                Height=103,
                 BackColor = Color.Firebrick,
                 TransparencyKey = Color.Firebrick,
-                Text = "Drag this to capture bounds"
+                Text = "Drag this to capture bounds, close to capture area, this can also be resized"
             };
             boundsFinder.FormClosing += GetFormBounds;
             Application.Run(boundsFinder);
             CaptureArea();
             if (hangOnCapturedBound) ShowCapture();
+            Console.WriteLine(generatedBounds);
         }
 
         private void CaptureArea()
@@ -51,6 +69,18 @@ namespace GFL_OCR_RSC_Tracker
             Bitmap captureBmp = new Bitmap(generatedBounds.Width, generatedBounds.Height);
             Graphics g = Graphics.FromImage(captureBmp);
             g.CopyFromScreen(generatedBounds.X, generatedBounds.Y, 0, 0, generatedBounds.Size);
+
+
+            for (int x = 0; x < captureBmp.Width; x++)
+            {
+                for (int y = 0; y < captureBmp.Height; y++)
+                {
+                    Color pixelColor = captureBmp.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(pixelColor.R > 128 ? 255 : 0, pixelColor.R > 128 ? 255 : 0, pixelColor.R > 128 ? 255 : 0);
+                    captureBmp.SetPixel(x, y, newColor);
+                }
+            }
+
             captureBmp.Save("Capture.png", ImageFormat.Png);
         }
 
